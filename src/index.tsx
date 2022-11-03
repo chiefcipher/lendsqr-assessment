f2713505554c12d78dc86b34ec13e2eb8d66ac3d
Samuel Yakubu <devwithsammy@gmail.com>
2022-11-03 18:57:28 +0100

    project init & clean up done

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+// TODO ADD HELMET                              
+// TODO ADD REDUX probably 
+// TODO REMOVE PUBLIC REACT CONTENT 
+// TODO ADD TECHNOLOGIES TO README.MD
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
